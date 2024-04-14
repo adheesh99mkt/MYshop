7888253ee7160aae3226a97a94aa8f31fc3eb346
adheesh99mkt <adheesh99mkt@gmail.com>
2024-04-14 14:11:21 +0530

    third commit database connected

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,18 @@ import express from "express";
 const app =express();
 // const dotenv =require ("dotenv");
 import dotenv from "dotenv";
+import { connectDatabase } from "./config/dbConnect.js";
 
 dotenv.config({path:"backend/config/config.env"});
+
+//connect to database
+
+connectDatabase()
 
 //import all routes
 
 import productRoutes from "./routes/products.js";
+
 app.use("/api/v1",productRoutes);
 
 app.listen(process.env.PORT,()=>{
