7683123d371b02cf437f0271bd47173349de08b3
adheesh99mkt <adheesh99mkt@gmail.com>
2024-04-14 13:28:25 +0530

    second commit

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
-const express =require ("express");
+// const express =require ("express");
+import express from "express";
 const app =express();
-const dotenv =require ("dotenv");
+// const dotenv =require ("dotenv");
+import dotenv from "dotenv";
 
 dotenv.config({path:"backend/config/config.env"});
+
+//import all routes
+
+import productRoutes from "./routes/products.js";
+app.use("/api/v1",productRoutes);
+
 app.listen(process.env.PORT,()=>{
     console.log(`server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });
